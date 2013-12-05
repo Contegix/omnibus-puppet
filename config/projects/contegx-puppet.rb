7f9246434adbd9c924ac8610ab44e80c53a39a74
christophergeers <christopher.geers@contegix.com>
2013-12-05 21:28:26 +0000

    no need for manual handling of config files now

--- config/projects/contegx-puppet.rb
+++ config/projects/contegx-puppet.rb
@@ -7,7 +7,7 @@ replaces        "contegix-puppet"
 install_path    "/opt/contegix/puppet"
 #build_version   Omnibus::BuildVersion.new.semver
 build_version   "3.3.2"
-build_iteration 11
+build_iteration 13
 
 # creates required build directories
 dependency "preparation"
@@ -16,6 +16,9 @@ dependency "preparation"
 dependency "ruby"
 dependency "puppet"
 
+config_file "#{install_path}/etc/puppet/puppet.conf"
+config_file "#{install_path}/etc/sysconfig/puppet"
+config_file "#{install_path}/etc/sysconfig/puppetmaster"
 
 # version manifest file
 dependency "version-manifest"
