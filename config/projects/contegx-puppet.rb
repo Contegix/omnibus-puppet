0222c26c82d9eaefc7a03cf0ad76dd0d4b0f7767
christophergeers <christopher.geers@contegix.com>
2013-12-05 19:16:23 +0000

    keep config files through upgrade

--- config/projects/contegx-puppet.rb
+++ config/projects/contegx-puppet.rb
@@ -7,7 +7,7 @@ replaces        "contegix-puppet"
 install_path    "/opt/contegix/puppet"
 #build_version   Omnibus::BuildVersion.new.semver
 build_version   "3.3.2"
-build_iteration 10
+build_iteration 11
 
 # creates required build directories
 dependency "preparation"
